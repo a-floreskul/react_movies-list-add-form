9e0614faf9a8fd66d0001a554801c11af480866c
Anton Floreskul <floreskula830@gmail.com>
2026-09-25 14:01:23 +0300

    some fixes

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
@@ -55,6 +55,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
     setImbUrl('');
     setImgUrl('');
     setimdbId('');
+    setCount(prevCount => prevCount + 1);
   };
 
   return (
